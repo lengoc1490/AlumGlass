a5e9b2fdee6f99b4558a790c877c1e94dc085d56
Le Van Ngoc <ngoclv@nxcom.vn>
2026-09-23 17:24:13 +0700

    Sửa bảng con

--- alumglass/alumglass/doctype/overrides/quotation.js
+++ alumglass/alumglass/doctype/overrides/quotation.js
@@ -508,23 +508,6 @@ alumglass.quotation.ItemParamDialog = class ItemParamDialog {
                 ],
             },
             { fieldname: "upload_section", fieldtype: "HTML", label: "Export/Import" },
-            {
-                fieldname: 'items',
-                fieldtype: 'Table',
-                name: 'Tieu chi',
-                label: __('Items'),
-                fields: [
-                    { 
-                        fieldname: 'purchase_receipt', 
-                        fieldtype: 'Link', 
-                        label: __('Purchase Receipt'), 
-                        options: "Purchase Receipt", 
-                        in_list_view: 1, 
-                        read_only: 0, 
-                        columns: 2, 
-                    },
-                ]
-            },
             { fieldtype: "Section Break", label: __("Kết quả Preview") },
             { fieldname: "preview_html", fieldtype: "HTML", label: "" },
         ];
@@ -576,7 +559,7 @@ alumglass.quotation.ItemParamDialog = class ItemParamDialog {
 
         alumglass.excel_io.attach(this.dialog, {
             section: "upload_section",
-            table: "items",
+            table: "extra_vars", // Sửa tên bảng con
             doctype: "Purchase Receipt",
             key: "purchase_receipt",
             // validator: "eupapp.eupapp.doctype.overrides.purchase_invoice.validate_receipt_rows",
